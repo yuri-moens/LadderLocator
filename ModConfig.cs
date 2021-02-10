eb53a502ed6af07ed81010939fc640992b3ac2e7
FinalDoom <677609+FinalDoom@users.noreply.github.com>
2021-02-10 05:52:56 -0700

    Additional customization settings/keybinds, small optimizations (#3)
    * Added a bunch of customization settings, more highlight types, some efficiency
    
    Co-authored-by: FinalDoom <7464170-FinalDoom@users.noreply.gitlab.com>

--- ModConfig.cs
+++ ModConfig.cs
@@ -1,11 +1,28 @@
-﻿using StardewModdingAPI;
+﻿using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Utilities;
+using System.Collections.Generic;
 
 namespace LadderLocator
 {
     class ModConfig
     {
-        public SButton ToggleShaftsKey { get; set; } = SButton.OemTilde;
+        public KeybindList ToggleShaftsKey { get; set; } = KeybindList.Parse("OemTilde");
         public bool ForceShafts { get; set; } = false;
+        public KeybindList ToggleHighlightTypeKey { get; set; } = KeybindList.Parse("LeftShift + OemTilde");
+        public HashSet<HighlightType> HighlightTypes { get; set; } = new HashSet<HighlightType>() { HighlightType.Rectangle };
+        public Color HighlightRectangleRGBA { get; set; } = Color.Lime;
+        public string HighlightImageFilename { get; set; } = "cracked.png";
+        public decimal HighlightAlpha { get; set; } = 0.45M;
+        public KeybindList CycleAlpha { get; set; } = KeybindList.Parse("LeftAlt + OemTilde");
+        public bool HighlightUsesStoneTint { get; set; } = false;
+        public KeybindList ToggleTint { get; set; } = KeybindList.Parse("LeftControl + OemTilde");
     }
 
+    enum HighlightType
+    {
+        Rectangle,
+        Image,
+        Sprite
+    }
 }
